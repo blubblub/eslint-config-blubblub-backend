f002107230f0ccaa96e3e216ed3487c662f81f10
Ziga Strgar <ziga.strgar@gmail.com>
2021-09-02 10:21:56 +0200

    Added typescript support

--- recommended.js
+++ recommended.js
@@ -1,9 +1,6 @@
 module.exports = {
     extends: [
         './index.js',
-        './jest.js',
-        './mongo.js',
-        './looseComments.js',
     ],
     rules: {
         'unicorn/require-post-message-target-origin': 0,
