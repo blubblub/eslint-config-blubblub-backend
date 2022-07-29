f9bb3742634eb3174ed540d5466e51aee7c95350
Marko Širec <m.sirec@gmail.com>
2022-07-29 14:48:55 +0200

    Remove darraghor's nestjs config because it was giving wrong paths

--- nestjs.js
+++ nestjs.js
@@ -2,12 +2,10 @@ module.exports = {
     env: {
         es6: true,
     },
-    extends: ['plugin:@darraghor/nestjs-typed/recommended'],
     parser: '@typescript-eslint/parser',
     parserOptions: {
         ecmaVersion: 'es2019',
         project: ['./tsconfig.json'],
         sourceType: 'module',
     },
-    plugins: ['@darraghor/nestjs-typed'],
 };
